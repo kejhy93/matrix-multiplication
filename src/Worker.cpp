09b777a49780ad2998dfb79be9d4289ccbafa241
Lukáš Hejnal <kejhy93@gmail.com>
2019-02-05 22:52:49 +0100

    Fix compilation issue

--- src/Worker.cpp
+++ src/Worker.cpp
@@ -13,6 +13,7 @@ Worker::~Worker() {
  * and write result to final matrix.
  */
 void Worker::do_job () {
+	std::cout << "Worker-1 starts" << std::endl;
 	// Run until task generation is finished and queue task is empty
 	// task generation condition ensure to not close worker method in case of slow task generation :)
 	while ( are_task_generated == 0 || m_queue->size() != 0 ) {
