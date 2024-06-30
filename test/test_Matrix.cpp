cb66dd476d0b6f72e2b32b5d789e7846cfde1e5a
Lukáš Hejnal <kejhy93@gmail.com>
2024-06-30 11:11:53 +0000

    Add test

--- test/test_Matrix.cpp
+++ test/test_Matrix.cpp
@@ -1,26 +1,29 @@
+#include <iostream>
+#include "lib/Matrix.h"
 #include "Matrix.h"
 
-const int FAILED_TEST = 1;
-const int SUCCESS_TEST = 0;
+const int FAILED_TEST = 0;
+const int SUCCESS_TEST = 1;
 
 int main() {
 
-    auto WIDTH = 2;
-    auto HEIGHT = 3;
+    int WIDTH = 2;
+    int HEIGHT = 3;
 
-    auto matrix = new Matrix(WIDTH, HEIGHT);
+    Matrix* matrix = new Matrix(WIDTH, HEIGHT);
 
-    auto width = matrix->get_col();
+    int width = matrix->get_col();
     if ( WIDTH != width) {
         std::cout << "Width is not correct. Expected " << WIDTH << ", got " << width << std::endl;
         return FAILED_TEST;
     }
 
-    auto height = matrix->get_row();
+    int height = matrix->get_row();
     if ( HEIGHT != height) {
         std::cout << "Height is not correct. Expected " << HEIGHT << ", got " << height << std::endl;
         return FAILED_TEST;
     }
 
+    delete matrix;
     return SUCCESS_TEST;
 }
