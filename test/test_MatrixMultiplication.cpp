ad81cfe8c17b4e4591e81e5d233a52e75ca100fe
Lukáš Hejnal <kejhy93@gmail.com>
2024-07-04 20:25:36 +0000

    Second test

---
+++ test/test_MatrixMultiplication.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#include "lib/MatrixMultiplication.h"
+#include "lib/Matrix.h"
+
+const int SUCCESS_TEST = 0;
+const int FAILED_TEST = 1;
+
+int main() {
+    return SUCCESS_TEST;
+}
