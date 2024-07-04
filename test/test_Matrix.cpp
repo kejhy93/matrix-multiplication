f745223da200d325d83b4575bc5c933a1fca455c
Lukáš Hejnal <kejhy93@gmail.com>
2024-07-04 20:24:58 +0000

    Fix my first test

--- test/test_Matrix.cpp
+++ test/test_Matrix.cpp
@@ -2,27 +2,43 @@
 #include "lib/Matrix.h"
 #include "Matrix.h"
 
-const int FAILED_TEST = 0;
-const int SUCCESS_TEST = 1;
+const int SUCCESS_TEST = 0;
+const int FAILED_TEST = 1;
 
-int main() {
+int test_matrixConstructor();
 
-    int WIDTH = 2;
-    int HEIGHT = 3;
+int main()
+{
+    std::cout << "TestMatrix" << std::endl;
+    int result = test_matrixConstructor();
+    if (result == FAILED_TEST)
+        return FAILED_TEST;
 
-    Matrix* matrix = new Matrix(WIDTH, HEIGHT);
+    return SUCCESS_TEST;
+}
+
+int test_matrixConstructor()
+{
+    int WIDTH = 2000;
+    int HEIGHT = 3000;
+
+    Matrix *matrix = new Matrix(HEIGHT, WIDTH);
 
     int width = matrix->get_col();
-    if ( WIDTH != width) {
+    if (WIDTH != width)
+    {
         std::cout << "Width is not correct. Expected " << WIDTH << ", got " << width << std::endl;
         return FAILED_TEST;
     }
+    std::cout << "Width is " << width << std::endl;
 
     int height = matrix->get_row();
-    if ( HEIGHT != height) {
+    if (HEIGHT != height)
+    {
         std::cout << "Height is not correct. Expected " << HEIGHT << ", got " << height << std::endl;
         return FAILED_TEST;
     }
+    std::cout << "Height is " << height << std::endl;
 
     delete matrix;
     return SUCCESS_TEST;
