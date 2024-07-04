0b9375f54c015728b96a32c5645aa1fb704fdf39
Lukáš Hejnal <kejhy93@gmail.com>
2024-07-04 20:37:31 +0000

    More test for matrix multiplication compatibility

--- test/test_MatrixMultiplication.cpp
+++ test/test_MatrixMultiplication.cpp
@@ -6,6 +6,52 @@
 const int SUCCESS_TEST = 0;
 const int FAILED_TEST = 1;
 
+int test_matricesCompatibleForMultiplication();
+
 int main() {
+
+    int result = test_matricesCompatibleForMultiplication();
+
+    return SUCCESS_TEST;
+}
+
+int test_matricesCompatibleForMultiplication() {
+    Matrix* leftMatrix = new Matrix(2, 3);
+    Matrix* rightMatrix = new Matrix(3 ,2);
+
+    bool areMatricesCompatible = MatrixMultiplication::areMatricesCompatibleForMultiplication(*leftMatrix, *rightMatrix);
+    if (areMatricesCompatible == false) {
+        std::cout << "Compatible matrices to multiple" << std::endl;
+        return FAILED_TEST; 
+    }
+    std::cout << "Compatible matrices check PASSED" << std::endl;
+
+    areMatricesCompatible = MatrixMultiplication::areMatricesCompatibleForMultiplication(*rightMatrix,*leftMatrix);
+    if (areMatricesCompatible == true) {
+        std::cout << "Incompatible matrices to multiple" << std::endl;
+        return FAILED_TEST;
+    }
+    std::cout << "Incompatible matrices check PASSED" << std::endl;
+    
+    delete leftMatrix;
+    delete rightMatrix;
+
+    leftMatrix = new Matrix(3, 3);
+    rightMatrix = new Matrix(3, 3);
+
+    areMatricesCompatible = MatrixMultiplication::areMatricesCompatibleForMultiplication(*rightMatrix,*leftMatrix);
+    if (areMatricesCompatible == true) {
+        std::cout << "Compatible matrices to multiple" << std::endl;
+        return FAILED_TEST;
+    }
+    std::cout << "Compatible matrices check PASSED" << std::endl;
+
+    areMatricesCompatible = MatrixMultiplication::areMatricesCompatibleForMultiplication(*rightMatrix,*leftMatrix);
+    if (areMatricesCompatible == true) {
+        std::cout << "Compatible matrices to multiple" << std::endl;
+        return FAILED_TEST;
+    }
+    std::cout << "Compatible matrices check PASSED" << std::endl;
+
     return SUCCESS_TEST;
 }
